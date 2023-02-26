c962a315a12ed536b28ae211f41cb8093c24d30f
lho05 <holokto@gmail.com>
2023-02-26 15:43:43 -0800

    Phase 1 Update 5 (phase complete fix 4)

--- src/test/model/StatsTest.java
+++ src/test/model/StatsTest.java
@@ -3,14 +3,21 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatsTest {
     private Stats stat1;
     private Stats stat2;
+    private String dateOfGame;
 
     @BeforeEach
     void init() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        dateOfGame = sdf.format(date);
         stat1 = new Stats(1, 2);
         stat2 = new Stats(3, 4);
 
@@ -22,6 +29,8 @@ public class StatsTest {
         assertEquals(2, stat1.getCPM());
         assertEquals(3, stat2.getWPM());
         assertEquals(4, stat2.getCPM());
+        assertEquals(dateOfGame, stat1.getDateOfGame());
+        assertEquals(dateOfGame, stat2.getDateOfGame());
 
     }
 
